e2f27fef73b1577cccfcae7f6ed99618692cfe91
Mashallah Behbehani <mkbehbehani@gmail.com>
2013-03-18 17:05:55 -0500

    Adds content for work.html

---
+++ Untitled Folder/js/mashallah.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    $('#menu a').bind('click',function(event){
+        var $anchor = $(this);
+ 
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top + 5
+        }, 2500,'easeInOutExpo');
+        event.preventDefault();
+    
+});
+    // $(function() {                     
+    //   $('#about4').waypoint(              
+    //     function() {
+    //       $('#about4').show();
+    //       }
+        
+    //   )});
+            $(function() {                   
+  $('.col').waypoint(
+  offset: 'bottom-in-view'              
+    function() {
+      $('#skill1').addClass('activebar1');
+      $('#skill2').addClass('activebar2');
+      $('#skill3').addClass('activebar3');
+      $('#skill4').addClass('activebar4');
+    }
+  )
+});
+// ANIMATING TEXT IN FROM SIDES
+    $(function() {   
+                    $("#about1").waypoint(function() {
+                    $("#aboutbackend").animate({"left": "0px"},"slow");});
+
+                 });
+    $(function() {   
+                    $("#about1").waypoint(function() {
+                    $("#aboutfrontend").animate({"right": "0px"},"slow");});
+
+                 });
+      $(function() {                     
+      $('#menu').waypoint(              
+        function() {
+          $('#menu').addClass('activenav').removeClass('nav');
+          }
+        
+      )});
+            
+    $("#login").hover(function () {
+          $('#login').hide('slow')
+          $('#circlecontent').show()  
+    });
+  
+
+       
+});
